1372025e3aa4661f5f010d885734b39dbe972256
samorso <Samuel.Orso@unige.ch>
2023-08-22 08:52:35 +0200

    update

--- .test/test_eigen.cpp
+++ .test/test_eigen.cpp
@@ -3,19 +3,29 @@
 #include <iterator>
 #include <algorithm>
 #include <vector>
-#include <Eigen/Dense>
+#include <eigen3/Eigen/Dense>
+
+using namespace Eigen;
 
 int main(){
-    //Eigen::VectorXi ivec = Eigen::VectorXi::LinSpaced(5,0,4);
-    std::vector<int> v(5);
-    std::iota(v.begin(), v.end(), 0);
-    std::random_device rd;
-    std::mt19937 g(rd());
-    std::shuffle(v.begin(), v.end(), g);
-    //Eigen::PermutationMatrix<Eigen::Dynamic,Eigen::Dynamic> perm(5);
-    //perm.setIdentity();
-    //std::random_shuffle(perm.indices().data(), perm.indices().data() + perm.indices().size());
-    //std::cout << v << std::endl;
-    std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, " "));
-    std::cout << "\n";
+    VectorXi v(5);
+    VectorXd x = VectorXd::Random(5);
+    v << 2, 3, 2, 1, 0;
+    std::cout << x << std::endl;
+    std::cout << v.unaryExpr(x) << std::endl;
 }
+
+// int main(){
+//     //Eigen::VectorXi ivec = Eigen::VectorXi::LinSpaced(5,0,4);
+//     std::vector<int> v(5);
+//     std::iota(v.begin(), v.end(), 0);
+//     std::random_device rd;
+//     std::mt19937 g(rd());
+//     std::shuffle(v.begin(), v.end(), g);
+//     //Eigen::PermutationMatrix<Eigen::Dynamic,Eigen::Dynamic> perm(5);
+//     //perm.setIdentity();
+//     //std::random_shuffle(perm.indices().data(), perm.indices().data() + perm.indices().size());
+//     //std::cout << v << std::endl;
+//     std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, " "));
+//     std::cout << "\n";
+// }
